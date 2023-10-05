fc331fd7b8ff504066d1eb4a3bb5051916418d92
Oluwafemi Adenuga <phemmylintry@gmail.com>
2023-10-05 12:32:41 +0100

    Work package client

--- openproject/client.py
+++ openproject/client.py
@@ -7,16 +7,17 @@ class Client:
         self.api_version = "v3"
         self.api_token = api_token
 
-    def _handle_response(self, response: httpx.Response) -> httpx.Response:
+    def _handle_response(self, response: httpx.Response):
         if response.status_code != 200:
             raise Exception(f"Error: {response.status_code}")
 
         response.raise_for_status()
-        return response
+        return response.json()
 
     def _send_request(
-        self, method: str, url: str, params=None, data=None, **kwargs
+        self, method: str, endpoint: str, params=None, data=None, **kwargs
     ) -> httpx.Response:
+        url = f"{self.base_url}/api/{self.api_version}/{endpoint}"
         with httpx.Client(auth=("apikey", self.api_token)) as client:
             response = client.request(method, url, params=params, json=data, **kwargs)
             return self._handle_response(response)
@@ -27,6 +28,16 @@ class WorkPackages:
         self.client = client
 
     def list(self):
-        url = f"{self.client.base_url}/api/{self.client.api_version}/work_packages"
-        response = self.client._send_request("GET", url)
-        return response.json()
+        return self.client._send_request("GET", "/work_packages")
+
+    def view(self, id: int):
+        return self.client._send_request("GET", f"/work_packages/{id}")
+
+    def create(self, data: dict):
+        return self.client._send_request("POST", "/work_packages", data=data)
+
+    def update(self, id: int, data: dict):
+        return self.client._send_request("PATCH", f"/work_packages/{id}", data=data)
+
+    def delete(self, id: int):
+        return self.client._send_request("DELETE", f"/work_packages/{id}")
