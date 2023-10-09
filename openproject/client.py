4f073019d3f873d60aaee71961484584d93fbc89
Oluwafemi Adenuga <phemmylintry@gmail.com>
2023-10-09 13:17:09 +0100

    Refactoring

--- openproject/client.py
+++ openproject/client.py
@@ -1,5 +1,6 @@
 import httpx
 from exceptions import APIError, AuthenticationError
+from constants import BASE_URL
 
 
 class Client:
@@ -21,14 +22,15 @@ class Client:
             return response.json()
         except:
             error = f"{response.text}"
-            if "application/json" in response.headers["Content-Type"]:
+            content_type = response.headers.get("Content-Type", "").lower()
+            if content_type == "application/json":
                 resp = response.json()
-                if "message" in resp:
+                error_message_keys = ("message", "msg")
+                error_message = next(
+                    (resp[key] for key in error_message_keys if key in resp), None
+                )
+                if error_message:
                     error = f"Message: {resp.get('message')} , Error details: {resp.get('errors')}, {resp.get('data')}"
-                    raise APIError(error, response)
-
-                elif "msg" in resp:
-                    error = f"Message: {resp.get('msg')} , Error details: {resp.get('errors')}, {resp.get('data')}"
                     raise APIError(error, response)
 
             raise APIError(error, response)
@@ -38,14 +40,14 @@ class Client:
     ) -> httpx.Response:
         url = f"{self.base_url}/api/{self.api_version}/{endpoint}"
         with httpx.Client(auth=("apikey", self.api_token)) as client:
-            if method == "DELETE":
-                headers = {"Content-Type": "application/hal+json"}
-                response = client.request(
-                    method, url, params=params, json=data, headers=headers, **kwargs
-                )
-                return self._handle_response(response)
+            headers = {"Content-Type": "application/json"}
 
-            response = client.request(method, url, params=params, json=data, **kwargs)
+            import pdb
+
+            pdb.set_trace()
+            response = client.request(
+                method, url, params=params, json=data, headers=headers, **kwargs
+            )
             return self._handle_response(response)
 
 
@@ -55,6 +57,25 @@ class SubClient(Client):
 
 
 class WorkPackages(SubClient):
+    _args_api_mapping = {
+        "_links": "_links",
+        "subject": "subject",
+        "description": "description",
+        "schedule_manually": "scheduleManually",
+        "readonly": "readonly",
+        "start_date": "startDate",
+        "due_date": "dueDate",
+        "derived_start_date": "derivedStartDate",
+        "derived_due_date": "derivedDueDate",
+        "estimated_time": "estimatedTime",
+        "derived_estimated_time": "derivedEstimatedTime",
+        "percentage_done": "percentageDone",
+        "custom_field_1": "customField1",
+        "custom_field_2": "customField2",
+        "created_at": "createdAt",
+        "updated_at": "updatedAt",
+    }
+
     def list(self):
         return self.client._send_request("GET", "/work_packages")
 
@@ -71,57 +92,64 @@ class WorkPackages(SubClient):
 
     def create(
         self,
-        _type: str = None,
         _links: dict = None,
         subject: str = None,
         description: str = None,
-        scheduleManually: bool = None,
+        schedule_manually: bool = None,
         readonly: bool = None,
-        startDate: str = None,
-        dueDate: str = None,
-        derivedStartDate: str = None,
-        derivedDueDate: str = None,
-        estimatedTime: str = None,
-        derivedEstimatedTime: str = None,
-        percentageDone: int = None,
-        customField1: str = None,
-        customField2: str = None,
-        createdAt: str = None,
-        updatedAt: str = None,
+        start_date: str = None,
+        due_date: str = None,
+        derived_start_date: str = None,
+        derived_due_date: str = None,
+        estimated_time: str = None,
+        derived_estimated_time: str = None,
+        percentage_done: int = None,
+        custom_field_1: str = None,
+        custom_field_2: str = None,
+        created_at: str = None,
+        updated_at: str = None,
         **kwargs,
     ):
         if _links is None and kwargs:
             _links = kwargs
 
-        data = self._process_data(locals())
+        data = {
+            self._args_api_mapping[k]: v
+            for k, v in locals().items()
+            if v is not None and k != "self" and k != "kwargs"
+        }
+
         return self.client._send_request("POST", "/work_packages", data=data)
 
     def update(
         self,
         id: int,
-        _type: str = None,
         _links: dict = None,
         subject: str = None,
         description: str = None,
-        scheduleManually: bool = None,
+        schedule_manually: bool = None,
         readonly: bool = None,
-        startDate: str = None,
-        dueDate: str = None,
-        derivedStartDate: str = None,
-        derivedDueDate: str = None,
-        estimatedTime: str = None,
-        derivedEstimatedTime: str = None,
-        percentageDone: int = None,
-        customField1: str = None,
-        customField2: str = None,
-        createdAt: str = None,
-        updatedAt: str = None,
+        start_date: str = None,
+        due_date: str = None,
+        derived_start_date: str = None,
+        derived_due_date: str = None,
+        estimated_time: str = None,
+        derived_estimated_time: str = None,
+        percentage_done: int = None,
+        custom_field_1: str = None,
+        custom_field_2: str = None,
+        created_at: str = None,
+        updated_at: str = None,
         **kwargs,
     ):
         if _links is None and kwargs:
             _links = kwargs
-
-        data = self._process_data(locals())
+        self._args_api_mapping["id"] = "id"
+        data = {
+            self._args_api_mapping[k]: v
+            for k, v in locals().items()
+            if v is not None and k != "self" and k != "kwargs"
+        }
         data.pop("id")
         return self.client._send_request(
             "PATCH", f"/work_packages/{id}", data=data
@@ -129,3 +157,18 @@ class WorkPackages(SubClient):
 
     def delete(self, id: int):
         return self.client._send_request("DELETE", f"/work_packages/{id}")
+
+
+client = Client(
+    BASE_URL, "51de856c3f58f5eca1302305f7f34ce52f87c3224be4eb9c2af0314d29acbb1c"
+)
+work_packages = WorkPackages(client)
+print(
+    work_packages.update(
+        id=1,
+        subject="Test",
+        project={"href": "/api/v3/projects/1"},
+        type={"href": "/api/v3/types/1"},
+        percentage_done=50,
+    )
+)
